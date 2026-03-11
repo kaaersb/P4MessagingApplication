fd0e32ed50bcde701fc33c6eee6686e96a213d87
kaare <kaaresb@proton.me>
2026-03-11 14:06:32 +0100

    Wagwan

--- main.py
+++ main.py
@@ -308,3 +308,7 @@ def serve_login():
 @app.get("/register.html")
 def serve_register():
     return FileResponse("static/register.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
